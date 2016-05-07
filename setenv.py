bf7c617cdb5498747de23430371ed4d28b6bd9fa
iotmon10 <iotthu2spring16@gmail.com>
2016-05-06 21:44:11 -0400

    Delete setenv.py

--- setenv.py
+++
@@ -1,7 +0,0 @@
-import sys,os
-
-os.environ['SPOTIPY_CLIENT_ID']='7f79ab0d8f0d4bb2a7672d8a0b82cc68'
-os.environ['SPOTIPY_CLIENT_SECRET']='3602cd63e46140c59d34b17a81e74893'
-os.environ['SPOTIPY_REDIRECT_URI']='http://donovanchan.wix.com/edisontest'
-
-
